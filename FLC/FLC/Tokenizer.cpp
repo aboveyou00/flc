075411512825f83854d499af7c1c76d568a23512
ksabry <khalidmee@gmail.com>
2015-10-20 23:56:29 -0600

    Additional tokenizer stuff

--- FLC/FLC/Tokenizer.cpp
+++ FLC/FLC/Tokenizer.cpp
@@ -4,33 +4,34 @@
 #include "CharacterLiteralToken.h"
 #include "FloatLiteralToken.h"
 #include "IntegerLiteralToken.h"
+#include "IdentifierToken.h"
 #include "ErrorToken.h"
+#include "EndOfFileToken.h"
 
 namespace flc
 {
     namespace tokens
     {
         Tokenizer::Tokenizer()
-        {
-            
+        {   
         }
 		Tokenizer::~Tokenizer()
 		{
-
 		}
 
-        vector<Token> Tokenizer::tokenize(istream *sourceFile, string path)
-        {
+		vector<Token> Tokenizer::tokenize(istream *sourceFile, string path)
+		{
 			vector<Token> *result = new vector<Token>();
 			int pos = 0;
-			endOfFile = false;
-			while (!endOfFile) {
+			while (!sourceFile->eof()) {
 				Token next = parseNextToken(sourceFile, &pos, path);
-				(*result).push_back(next);
+				result->push_back(next);
 				pos += next.getLength();
+				if (sourceFile->eof())
+					result->push_back(EndOfFileToken(path, pos));
 			}
 			return *result;
-        }
+		}
 		
 		bool Tokenizer::isWhiteSpace(char c) {
 			return (c == ' ' || c == '\t' || c == '\n' || c == '\r');
@@ -41,13 +42,19 @@ namespace flc
 		}
 
 		Token Tokenizer::parseNextToken(istream *source, int *index, string path) {
-			while (isWhiteSpace((*source).peek())) {
-				index++;(*source).get();
+			while (isWhiteSpace(source->peek())) {
+				index++;source->get();
 			}
-			
-			char nextChar = (*source).peek();
+			if (source->eof()) {
+				return EndOfFileToken(path, *index);
+			}
+
+			char nextChar = source->peek();
 			if (nextChar == '\'') {
 				return parseCharacterToken(source, index, path);
+			}
+			else if (nextChar == '"') {
+				return parseStringToken(source, index, path);
 			}
 			else if (nextChar >= '0' && nextChar <= '9') {
 				return parseNumericToken(source, index, path);
@@ -60,22 +67,30 @@ namespace flc
 			}
 		}
 		Token Tokenizer::parseCharacterToken(istream *source, int *index, string path) {
-			(*source).get();
-			char nextChar = (*source).peek();
+			source->get();
+			if (source->eof())
+				return ErrorToken(path, *index, 1, "EOF while parsing char literal");
+
+			char nextChar = source->peek();
 			if (nextChar == '\\') {
 				int length = 3;
 				stringstream escapeSequence;
 				escapeSequence << nextChar;		// get at least one character
-				while ((nextChar = (*source).get()) != '\'') {
+				while ((nextChar = source->get()) != '\'') {
 					escapeSequence << nextChar;
 					length++;
+					if (source->eof())
+						return ErrorToken(path, *index, length - 1, "EOF while parsing char literal");
 				}
 				char c = escapeSequenceToChar(escapeSequence.str());
 				return CharacterLiteralToken(path, *index, length, c);
 			}
 			else {
-				char c = (*source).get();
-				if ((*source).get() != '\'') {
+				char c = source->get();
+				if (source->eof()) {
+					return ErrorToken(path, *index, 2, "EOF while parsing char literal");
+				}
+				else if (source->get() != '\'') {
 					return ErrorToken(path, *index, 3, "Invalid char literal");
 				}
 				else {
@@ -88,7 +103,7 @@ namespace flc
 			int length = 0;
 			bool foundDecimal = false;
 			char nextChar;
-			while ( (nextChar = (*source).get()) && (nextChar >= '0' && nextChar <= '9' || (nextChar == '.' && !foundDecimal)) ) {
+			while ( !source->eof() && (nextChar = source->get()) && (nextChar >= '0' && nextChar <= '9' || (nextChar == '.' && !foundDecimal)) ) {
 				if (nextChar == '.')
 					foundDecimal = true;
 				numberString << nextChar;
@@ -104,7 +119,16 @@ namespace flc
 
 		Token Tokenizer::parseStringToken(istream *source, int *index, string path) {}
 
-		Token Tokenizer::parseIdentifierToken(istream *source, int *index, string path) {}
+		Token Tokenizer::parseIdentifierToken(istream *source, int *index, string path) {
+			stringstream identString;
+			int length = 0;
+			char nextChar;
+			while (!source->eof() && (nextChar = source->get()) && (nextChar >= 'a' && nextChar <= 'z' || nextChar >= 'A' && nextChar <= 'Z' || nextChar >= '0' && nextChar <= '9' || nextChar == '_')) {
+				identString << nextChar;
+				length++;
+			}
+			return IdentifierToken(path, *index, length, identString.str());
+		}
 
 		Token Tokenizer::parseSymbolToken(istream *source, int *index, string path) {}
     }
--- FLC/FLC/Tokenizer.h
+++ FLC/FLC/Tokenizer.h
@@ -15,7 +15,6 @@ namespace flc
 			vector<Token> tokenize(istream * sourceFile, string path="");
 		
 		private:
-			bool endOfFile;
 			Token parseNextToken(istream *source, int *index, string path);
 			Token parseCharacterToken(istream *source, int *index, string path);
 			Token parseNumericToken(istream *source, int *index, string path);
