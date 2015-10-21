e3f3350220a25345ae6dbe8bc326c731f67e0125
ksabry <khalidmee@gmail.com>
2015-10-20 21:19:29 -0600

    Early stages of tokenizer

--- FLC/FLC/Tokenizer.cpp
+++ FLC/FLC/Tokenizer.cpp
@@ -1,5 +1,7 @@
 #include "stdafx.h"
 #include "Tokenizer.h"
+#include "CharacterLiteralToken.h"
+#include "ErrorToken.h"
 
 namespace flc
 {
@@ -9,15 +11,83 @@ namespace flc
         {
             
         }
-        Tokenizer::~Tokenizer()
-        {
+		Tokenizer::~Tokenizer()
+		{
 
-        }
+		}
 
-        vector<Token> Tokenizer::tokenize(istream *sourceFile)
+        vector<Token> Tokenizer::tokenize(istream *sourceFile, string path)
         {
-            reportNotImplemented("Tokenizer::tokenize");
-            throw 20;
+			int pos = 0;
+			char strBuffer[256];
+			bool reading = true;
+			while (reading) {
+				(*sourceFile).get(strBuffer,256);
+				Token next = parseNextToken(strBuffer, pos, path);
+			}
         }
+		
+		bool Tokenizer::isWhiteSpace(char c) {
+			return (c == ' ' || c == '\t' || c == '\n' || c == '\r');
+		}
+		char escapeSequenceToChar(char* sequence) {
+			// Not implemented
+			return sequence[0];
+		}
+
+		Token Tokenizer::parseNextToken(char *buffer, int startPos, string path) {
+			int index = 0;
+			for (; index < 256; index++) {
+				if (!isWhiteSpace(buffer[index])) {
+					break;
+				}
+			}
+			int length;
+			if (buffer[index] == '\'') {
+				char c;
+				if (parseCharacterToken(buffer, &length, &c)) {
+					return CharacterLiteralToken(path, startPos, length, c);
+				}
+			}
+
+
+			
+			return ErrorToken(path, startPos, length, "Invalid character literal");
+		}
+		bool Tokenizer::parseCharacterToken(char *buffer, int *length, char *c) {
+			if (buffer[1] == '\\') {
+				int index = 1;
+				char escapeSequence[256]; escapeSequence[0] = buffer[1];	// get at least one character
+				while (buffer[index] != '\'') {
+					escapeSequence[index] = buffer[index + 2];
+					index++;
+				}
+				char c = escapeSequenceToChar(escapeSequence);
+				*length = index + 3;
+				return true;
+			}
+			else {
+				if (buffer[2] != '\'') {
+					flc::reportError("Invalid char literal");
+					*length = 2;
+					return false;
+				}
+				else {
+					*length = 3;
+					*c = buffer[1];
+					return true;
+				}
+			}
+		}
+		bool Tokenizer::parseFloatLiteralToken(char *buffer, int* length) {}
+
+		bool Tokenizer::parseIntegerLiteralToken(char *buffer, int* length) {}
+
+		bool Tokenizer::parseStringLiteralToken(char *buffer, int* length) {}
+
+		bool Tokenizer::parseSymbolLiteralToken(char *buffer, int* length) {}
+
+		bool Tokenizer::parseIdentifierToken(char *buffer, int* length) {}
+
     }
 }
--- FLC/FLC/Tokenizer.h
+++ FLC/FLC/Tokenizer.h
@@ -12,7 +12,18 @@ namespace flc
             Tokenizer();
             ~Tokenizer();
 
-            vector<Token> tokenize(istream *sourceFile);
+			vector<Token> tokenize(istream * sourceFile, string path="");
+		
+		private:
+			Token parseNextToken(char *buffer, int startPos, string path);
+			bool parseCharacterToken(char *buffer, int *length, char *c);
+			bool parseFloatLiteralToken(char *buffer, int *length);
+			bool parseIntegerLiteralToken(char * buffer, int * length);
+			bool parseStringLiteralToken(char * buffer, int * length);
+			bool parseSymbolLiteralToken(char * buffer, int * length);
+			bool parseIdentifierToken(char * buffer, int * length);
+			bool isWhiteSpace(char c);
+
         };
     }
 }
