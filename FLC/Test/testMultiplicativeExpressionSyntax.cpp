8d628bbeaccc51d7102f6579bd3c69ba12b32cde
aboveyou00 <brandonyoyoslade@gmail.com>
2016-04-14 21:49:33 -0600

    Added more broken multiplicative tests

--- FLC/Test/testMultiplicativeExpressionSyntax.cpp
+++ FLC/Test/testMultiplicativeExpressionSyntax.cpp
@@ -13,15 +13,78 @@ namespace Test
         {
             testExpressionType("1 * 2", RuntimeType::int32);
         }
+        TEST_METHOD(Test_multiplicative_exprType_multiplyUInt32)
+        {
+            testExpressionType("1u * 2u", RuntimeType::int32);
+        }
+        TEST_METHOD(Test_multiplicative_exprType_multiplyInt64)
+        {
+            testExpressionType("1l * 2l", RuntimeType::int64);
+        }
+        TEST_METHOD(Test_multiplicative_exprType_multiplyUInt64)
+        {
+            testExpressionType("1ul * 2ul", RuntimeType::int64);
+        }
+
+        TEST_METHOD(Test_multiplicative_exprType_multiplyFloat)
+        {
+            testExpressionType("1f * 2f", RuntimeType::float32);
+        }
+        TEST_METHOD(Test_multiplicative_exprType_multiplyDouble)
+        {
+            testExpressionType("1d * 2d", RuntimeType::float64);
+        }
 
         TEST_METHOD(Test_multiplicative_exprType_divideInt32)
         {
             testExpressionType("1 / 2", RuntimeType::int32);
         }
+        TEST_METHOD(Test_multiplicative_exprType_divideUInt32)
+        {
+            testExpressionType("1u / 2u", RuntimeType::int32);
+        }
+        TEST_METHOD(Test_multiplicative_exprType_divideInt64)
+        {
+            testExpressionType("1l / 2l", RuntimeType::int64);
+        }
+        TEST_METHOD(Test_multiplicative_exprType_divideUInt64)
+        {
+            testExpressionType("1ul / 2ul", RuntimeType::int64);
+        }
+
+        TEST_METHOD(Test_multiplicative_exprType_divideFloat)
+        {
+            testExpressionType("1f / 2f", RuntimeType::float32);
+        }
+        TEST_METHOD(Test_multiplicative_exprType_divideDouble)
+        {
+            testExpressionType("1d / 2d", RuntimeType::float64);
+        }
 
         TEST_METHOD(Test_multiplicative_exprType_modulusInt32)
         {
             testExpressionType("1 % 2", RuntimeType::int32);
+        }
+        TEST_METHOD(Test_multiplicative_exprType_modulusUInt32)
+        {
+            testExpressionType("1u % 2u", RuntimeType::int32);
+        }
+        TEST_METHOD(Test_multiplicative_exprType_modulusInt64)
+        {
+            testExpressionType("1l % 2l", RuntimeType::int64);
+        }
+        TEST_METHOD(Test_multiplicative_exprType_modulusUInt64)
+        {
+            testExpressionType("1ul % 2ul", RuntimeType::int64);
+        }
+
+        TEST_METHOD(Test_multiplicative_exprType_modulusFloat)
+        {
+            testExpressionType("1f % 2f", RuntimeType::float32);
+        }
+        TEST_METHOD(Test_multiplicative_exprType_modulusDouble)
+        {
+            testExpressionType("1d % 2d", RuntimeType::float64);
         }
     };
 }
